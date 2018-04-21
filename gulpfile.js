2d55d3f88c5ebf0d5b671e7301d8ed28a9ce56f4
Aleksander Fret <alemik@op.pl>
2018-04-21 19:20:27 +0200

    Edit: gulp.watch to html files changes

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ const htmlreplace = require('gulp-html-replace');
 gulp.task('default', function(done){
   gulp.watch('./app/sass/**/*.scss', gulp.series('styles'));
   gulp.watch('./app/src/**/*.js', gulp.series('js'));
-  gulp.watch('.app/index.html', gulp.series('html'));
+  gulp.watch('./app/index.html', gulp.series('html'));
   browserSync.init({
     server: "./app"
   });
@@ -54,12 +54,8 @@ gulp.task('js', (done) => {
     gulp.src('./app/index.html')
     .pipe(htmlreplace({
       'js': 'js/main.min.js'
-    }));
-    done();
-  });
-
-  gulp.task('images', (done) => {
-    gulp.src();
+    }))
+    .pipe(browserSync.stream());
     done();
   });
 
