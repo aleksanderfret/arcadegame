590f13e7c733a165d676416a8391fa390c2da9ce
Aleksander Fret <alemik@op.pl>
2018-04-24 23:56:54 +0200

    Hermetization: Game and Level classes.

--- app/src/app.js
+++ app/src/app.js
@@ -12,11 +12,6 @@ class GameEntity {
     }
   }
 
-  addExtension(string){
-    const exetension = '.png'
-    return string+extension;
-  }
-
   getEdge(edge) {
     const coord = (['left', 'right'].includes(edge)) ? 'x' : 'y';
     return this[coord] + this.edges[edge];
@@ -92,329 +87,410 @@ class Enemy extends GameEntity {
   }
 }
 
-class Player extends GameEntity {
-  constructor(sprite) {
-    super();
-    this.reset();
-    this.minX = 0;
-    this.minY = -10;
-    this.maxX = 404;
-    this.maxY = 405;
-    this.sprite = sprite;
-    let lives = 3;
-    this.edges = {
-      left: 20,
-      right: 81,
-      top: 60,
-      bottom: 143
-    }
-    this.getLives = () => {
-      return lives;
-    }
-    this.decreaseLives = () => {
-      lives = (lives > 0) ? lives - 1 : lives;
-    }
-    document.addEventListener('keyup', (event) => {
-      const allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-      };
-      this.movementHandlerInput(allowedKeys[event.keyCode]);
-    });
-
-  }
-
-  reset() {
-    this.isDead = false;
-    this.x = 202;
-    this.y = 405;
-  }
-
-  update() {}
-
-  die() {
-    this.isDead = true;
-  }
-
-  spriteAnimetion(result) {
-    let interval;
-    const spriteNormal = this.sprite.normal;
-    const spriteAnimated = (result === 'win') ? this.sprite.win : this.sprite.lose;
-    interval = setInterval(() => {
-      this.sprite.normal = (this.sprite.normal === spriteAnimated) ? spriteNormal : spriteAnimated;
-    }, 100);
-    setTimeout(()=> {
-      clearInterval(interval);
-      this.sprite.normal = spriteNormal;
-    }, 750);
-  }
-
-  render() {
-    ctx.drawImage(Resources.get(this.sprite.normal), this.x, this.y);
-  }
-
-  movementHandlerInput(direction) {
-    if (this.isDead) {
-      return;
-    }
-    switch (direction) {
-      case 'left':
-        this.x = Math.max((this.x - this.stepX), this.minX);
-        break;
-      case 'right':
-        this.x = Math.min((this.x + this.stepX), this.maxX);
-        break;
-      case 'up':
-        this.y = Math.max((this.y - this.stepY), this.minY);
-        break;
-      case 'down':
-        this.y = Math.min((this.y + this.stepY), this.maxY);
-        break;
-      default:
-        null;
-    }
-  }
-}
-
-class Game {
-  constructor() {
-    this.gamePanel = new GamePanel();
-    this.startIntro();
-    this.enemies = [];
-    this.maxLevel = 3;
-    this.isLock = false;
-    this.isStarted = false;
-  }
-  getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  createEnemies() {
-    const createdEnemies = [];
-    const enemiesNumberRange = this.level.getEnemiesNumberRange();
-    const enemiesSpeedRange = this.level.getEnemiesSpeedRange();
-    const enemiesNum = this.getRandomInt(enemiesNumberRange[0], enemiesNumberRange[1]);
-    const newEnemiesNum = enemiesNum - this.enemies.length;
-    let isOppositeEnemy = 0;
-    let enemyTrack;
-    let enemySpeed;
-    for (let i = 1; i <= newEnemiesNum; i++) {
-      if (this.enemies.length >= 4) {
-        isOppositeEnemy = this.getRandomInt(0, 1);
+const gamePlayer = ((sprite) => {
+  "Use strict";
+  const priv = new WeakMap();
+  const _= (instance) => priv.get(instance);
+  class Player extends GameEntity {
+    constructor(sprite) {
+      super();
+      const privateProps = {
       }
-      enemySpeed = this.getRandomInt(enemiesSpeedRange[0], enemiesSpeedRange[1]);
+      priv.set(this, privateProps);
+      this.reset();
+      this.minX = 0;
+      this.minY = -10;
+      this.maxX = 404;
+      this.maxY = 405;
+      this.sprite = sprite;
+      let lives = 3;
+      this.edges = {
+        left: 20,
+        right: 81,
+        top: 60,
+        bottom: 143
+      }
+      this.getLives = () => {
+        return lives;
+      }
+      this.decreaseLives = () => {
+        lives = (lives > 0) ? lives - 1 : lives;
+      }
+      document.addEventListener('keyup', (event) => {
+        const allowedKeys = {
+          37: 'left',
+          38: 'up',
+          39: 'right',
+          40: 'down'
+        };
+        this.movementHandlerInput(allowedKeys[event.keyCode]);
+      });
 
-      if (this.enemies.length === 0 && i <= 3) {
-        enemyTrack = i;
+    }
+
+    reset() {
+      this.isDead = false;
+      this.x = 202;
+      this.y = 405;
+    }
+
+    update() {}
+
+    die() {
+      this.isDead = true;
+    }
+
+    spriteAnimetion(result) {
+      let interval;
+      const spriteNormal = this.sprite.normal;
+      const spriteAnimated = (result === 'win') ? this.sprite.win : this.sprite.lose;
+      interval = setInterval(() => {
+        this.sprite.normal = (this.sprite.normal === spriteAnimated) ? spriteNormal : spriteAnimated;
+      }, 100);
+      setTimeout(()=> {
+        clearInterval(interval);
+        this.sprite.normal = spriteNormal;
+      }, 750);
+    }
+
+    render() {
+      ctx.drawImage(Resources.get(this.sprite.normal), this.x, this.y);
+    }
+
+    movementHandlerInput(direction) {
+      if (this.isDead) {
+        return;
+      }
+      switch (direction) {
+        case 'left':
+          this.x = Math.max((this.x - this.stepX), this.minX);
+          break;
+        case 'right':
+          this.x = Math.min((this.x + this.stepX), this.maxX);
+          break;
+        case 'up':
+          this.y = Math.max((this.y - this.stepY), this.minY);
+          break;
+        case 'down':
+          this.y = Math.min((this.y + this.stepY), this.maxY);
+          break;
+        default:
+          null;
+      }
+    }
+  }
+  return Player;
+})();
+
+const arcadeGame = (()=>{
+  "Use strict";
+  const priv = new WeakMap();
+  const _= (instance) => priv.get(instance);
+  const setLocked = (instance, locked) => {
+    _(instance).isLocked = locked;
+  }
+  const setIsStarted = (instance, started) => {
+    _(instance).isStarted = started;
+  };
+  const setPlayer = (instance, player) => {
+    _(instance).player = player;
+  }
+  const setLevel = (instance, level) => {
+    _(instance).level = level;
+  }
+  const setEnemies = (instance, enemies) => {
+    _(instance).enemies = enemies;
+  };
+
+  class Game {
+    constructor() {
+      const privateProps = {
+        gamePanel: new GamePanel(),
+        isStarted: false,
+        isLocked: false,
+        level: null,
+        player: null,
+        maxLevel: 3,
+        enemies: [],
+      }
+      priv.set(this, privateProps);
+      this.startIntro();
+    }
+    get isStarted(){
+      return _(this).isStarted;
+    }
+    get isLocked(){
+      return _(this).isLocked;
+    }
+    get gamePanel(){
+      return _(this).gamePanel;
+    }
+    get level(){
+      return _(this).level;
+    }
+    get player(){
+      return _(this).player;
+    }
+    get enemies(){
+      return [..._(this).enemies];
+    }
+    get maxLevel(){
+      return _(this).maxLevel;
+    }
+    getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    createEnemies() {
+      const createdEnemies = [];
+      const enemiesNumberRange = this.level.enemiesNumberRange;
+      const enemiesSpeedRange = this.level.enemiesSpeedRange;
+      const enemiesNum = this.getRandomInt(enemiesNumberRange[0], enemiesNumberRange[1]);
+      const newEnemiesNum = enemiesNum - this.enemies.length;
+      let isOppositeEnemy = 0;
+      let enemyTrack;
+      let enemySpeed;
+      for (let i = 1; i <= newEnemiesNum; i++) {
+        if (this.enemies.length >= 4) {
+          isOppositeEnemy = this.getRandomInt(0, 1);
+        }
+        enemySpeed = this.getRandomInt(enemiesSpeedRange[0], enemiesSpeedRange[1]);
+
+        if (this.enemies.length === 0 && i <= 3) {
+          enemyTrack = i;
+        } else {
+          enemyTrack = this.getRandomInt(1, 3);
+        }
+
+        const startEdge = isOppositeEnemy ? 'right' : 'left';
+        createdEnemies.push(new Enemy(enemyTrack, enemySpeed, startEdge));
+      }
+      setEnemies(this, [...this.enemies, ...createdEnemies]);
+    }
+    render() {
+      if(this.isStarted){
+        this.renderEntities();
+        this.gamePanel.render(this.level.level, this.player.getLives());
+      }
+    }
+    renderEntities() {
+      this.enemies.forEach(function (enemy) {
+        enemy.render();
+      });
+      this.player.render();
+    }
+    checkIsDaed() {
+      let isDead = false;
+      this.player.die();
+      this.player.decreaseLives();
+
+      if (this.player.getLives() === 0 ) {
+        isDead = true;
       } else {
-        enemyTrack = this.getRandomInt(1, 3);
+        this.player.spriteAnimetion('lose');
       }
-
-      const startEdge = isOppositeEnemy ? 'right' : 'left';
-      createdEnemies.push(new Enemy(enemyTrack, enemySpeed, startEdge));
+      return isDead;
     }
-    this.enemies.push(...createdEnemies);
-
-  }
-  render() {
-    if(this.isStarted){
-      this.renderEntities();
-      this.gamePanel.render(this.level.getLevel(), this.player.getLives());
+    update(dt) {
+      if(!this.isStarted){
+        return;
+      }
+      this.updateEntities(dt);
+      if (this.isLocked) {
+        return
+      }
+      if (this.checkCollision()) {
+        setLocked(this, true);
+        if (this.checkIsDaed()) {
+          this.loseGame();
+        } else {
+          this.resetRound();
+        }
+      } else if (this.checkGoal()) {
+        setLocked(this, true);
+        if (this.level.level === this.maxLevel) {
+          this.winGame();
+        } else {
+          this.increaseLevel();
+        }
+      }
     }
-  }
-  renderEntities() {
-    this.enemies.forEach(function (enemy) {
-      enemy.render();
-    });
-    this.player.render();
-  }
-  checkIsDaed() {
-    let isDead = false;
-    this.player.die();
-    this.player.decreaseLives();
-
-    if (this.player.getLives() === 0 ) {
-      isDead = true;
-    } else {
+    updateEntities(dt) {
+      this.enemies.forEach((enemy) => {
+        enemy.update(dt);
+      });
+      this.player.update();
+    }
+    increaseLevel() {
+      this.level.update();
+      this.enemies.forEach((enemy) => {
+        const speedRange = this.level.enemiesNumberRange;
+        enemy.increaseSpeed(this.getRandomInt(speedRange[0], speedRange[1]));
+      });
+      this.player.spriteAnimetion('win');
+      this.resetRound();
+    }
+    loseGame() {
+      this.enemies.forEach((enemy) => {
+        enemy.stop();
+      });
       this.player.spriteAnimetion('lose');
-    }
-    return isDead;
-  }
-  update(dt) {
-    if(!this.isStarted){
-      return;
-    }
-    this.updateEntities(dt);
-    if (this.isLock) {
-      return
-    }
-    if (this.checkCollision()) {
-      this.isLock = true;
-      if (this.checkIsDaed()) {
-        this.loseGame();
-      } else {
-        this.resetRound();
-      }
-    } else if (this.checkGoal()) {
-      this.isLock = true;
-      if (this.level.getLevel() === this.maxLevel) {
-        this.winGame();
-      } else {
-        this.increaseLevel();
-      }
-    }
-  }
-  updateEntities(dt) {
-    this.enemies.forEach((enemy) => {
-      enemy.update(dt);
-    });
-    this.player.update();
-  }
-  increaseLevel() {
-    this.level.update();
-    this.enemies.forEach((enemy) => {
-      const speedRange = this.level.getEnemiesNumberRange();
-      enemy.increaseSpeed(this.getRandomInt(speedRange[0], speedRange[1]));
-    });
-    this.player.spriteAnimetion('win');
-    this.resetRound();
-  }
-  loseGame() {
-    this.enemies.forEach((enemy) => {
-      enemy.stop();
-    });
-    this.player.spriteAnimetion('lose');
-    setTimeout(()=>{
-      this.gamePanel.displayResultModal(false, this.level.getLevel(), this.player.getLives(), 0, 0);
-      this.stopGame();
-    }, 1000);
+      setTimeout(()=>{
+        this.gamePanel.displayResultModal(false, this.level.level, this.player.getLives(), 0, 0);
+        this.stopGame();
+      }, 1000);
 
-  }
-  winGame() {
-    this.enemies.forEach((enemy) => {
-      enemy.stop();
-    });
-    this.player.spriteAnimetion('win');
-    setTimeout(()=>{
-      this.gamePanel.displayResultModal(true, this.level.getLevel(), this.player.getLives(), 0, 0);
-      this.stopGame();
-    }, 1000);
-  }
-  startGame(sprite) {
-    this.level = new Level();
-    this.createEnemies();
-    this.player = new Player(sprite);
-    this.isStarted = true;
-    this.isLock = false;
-  }
-  stopGame() {
-    this.isStarted = false;
-    this.enemies = [];
-    this.player = null;
-    this.level = null;
-  }
-  startIntro(){
-    this.gamePanel.displayIntroModal();
-  }
+    }
+    winGame() {
+      this.enemies.forEach((enemy) => {
+        enemy.stop();
+      });
+      this.player.spriteAnimetion('win');
+      setTimeout(()=>{
+        this.gamePanel.displayResultModal(true, this.level.level, this.player.getLives(), 0, 0);
+        this.stopGame();
+      }, 1000);
+    }
+    startGame(sprite) {
+      setLevel(this, new gameLevel());
+      this.createEnemies();
+      setPlayer(this, new gamePlayer(sprite));
+      setIsStarted(this, true);
+      setLocked(this, false)
+    }
+    stopGame() {
+      setIsStarted(this, false);
+      setEnemies(this, []);
+      setPlayer(this, null);
+      setLevel(this, null);
+    }
+    startIntro(){
+      this.gamePanel.displayIntroModal();
+    }
 
-  checkCollision() {
-    for (const enemy of this.enemies) {
-      if (this.player.getEdge('left') <= enemy.getEdge('right') &&
-        this.player.getEdge('right') >= enemy.getEdge('left') &&
-        this.player.getEdge('top') <= enemy.getEdge('bottom') &&
-        this.player.getEdge('bottom') >= enemy.getEdge('top')) {
+    checkCollision() {
+      for (const enemy of this.enemies) {
+        if (this.player.getEdge('left') <= enemy.getEdge('right') &&
+          this.player.getEdge('right') >= enemy.getEdge('left') &&
+          this.player.getEdge('top') <= enemy.getEdge('bottom') &&
+          this.player.getEdge('bottom') >= enemy.getEdge('top')) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    checkGoal() {
+      if (this.player.y == this.player.minY) {
         return true;
       }
     }
-    return false;
+    resetRound() {
+      setTimeout(() => {
+        this.createEnemies();
+        this.player.reset();
+        setLocked(this, false);
+      }, 1000);
+    }
+  }
+  return Game;
+})();
+
+const gameLevel = (() => {
+  "Use strict";
+  const priv = new WeakMap();
+  const _= (instance) => priv.get(instance);
+  const setLevel = (instance) => {
+    _(instance).level++;
+  }
+  const setEnemiesNumberRange = (instance, numberRange) => {
+    _(instance).enemiesNumberRange = numberRange;
+  }
+  const setEnemiesSpeedRange = (instance, speedRange) => {
+    _(instance).enemiesSpeedRange = speedRange;
   }
 
-  checkGoal() {
-    if (this.player.y == this.player.minY) {
-      return true;
+  class Level {
+    constructor() {
+      const initLevel = 1;
+      const privateProps = {
+        level: initLevel,
+        enemiesNumberRange: this.calculateEnemiesNumber(initLevel),
+        enemiesSpeedRange: this.calculateEnemiesSpeed(initLevel),
+      }
+      priv.set(this, privateProps);
     }
-  }
-  resetRound() {
-    setTimeout(() => {
-      this.createEnemies();
-      this.player.reset();
-      this.isLock = false;
-    }, 1000);
-  }
-}
 
-class Level {
-  constructor() {
-    let level = 1;
-    let enemiesNumberRange = this.calculateEnemiesNumber(level);
-    let enemiesSpeedRange = this.calculateEnemiesSpeed(level);
-    this.getLevel = () => level;
-    this.setLevel = () => {
-      level++;
-      enemiesNumberRange = this.calculateEnemiesNumber(level);
-      enemiesSpeedRange = this.calculateEnemiesSpeed(level);
-    };
-    this.getEnemiesNumberRange = () => enemiesNumberRange;
-    this.getEnemiesSpeedRange = () => enemiesSpeedRange;
-  }
-  update() {
-    this.setLevel();
-  }
-  calculateEnemiesNumber(level) {
-    const enemiesNumberRange = [];
-    let min;
-    let max;
-    if (level <= 2) {
-      min = 3;
-      max = 3;
-    } else if (level <= 4) {
-      min = 4;
-      max = 5;
-    } else if (level <= 6) {
-      min = 5;
-      max = 7;
-    } else if (level <= 8) {
-      min = 6;
-      max = 8;
-    } else if (level <= 10) {
-      min = 7;
-      max = 10;
-    } else {
-      min = 8;
-      max = 12;
+    get level(){
+      return _(this).level;
     }
-    enemiesNumberRange.push(min, max);
-    return enemiesNumberRange;
-  }
-  calculateEnemiesSpeed(level) {
-    const enemiesSpeedRange = [];
-    let min;
-    let max;
-    if (level <= 2) {
-      min = 30;
-      max = 100;
-    } else if (level <= 4) {
-      min = 40;
-      max = 140;
-    } else if (level <= 6) {
-      min = 50;
-      max = 180;
-    } else if (level <= 8) {
-      min = 60;
-      max = 220;
-    } else if (level <= 10) {
-      min = 70;
-      max = 260;
-    } else {
-      min = 80;
-      max = 300;
+    get enemiesNumberRange(){
+      return _(this).enemiesNumberRange;
     }
-    enemiesSpeedRange.push(min, max);
-    return enemiesSpeedRange;
+    get enemiesSpeedRange(){
+      return _(this).enemiesSpeedRange;
+    }
+
+    update() {
+      setLevel(this);
+      setEnemiesNumberRange(this, this.calculateEnemiesNumber(this.level));
+      setEnemiesSpeedRange(this, this.calculateEnemiesSpeed(this.level));
+    }
+    calculateEnemiesNumber(level) {
+      const enemiesNumberRange = [];
+      let min;
+      let max;
+      if (level <= 2) {
+        min = 3;
+        max = 3;
+      } else if (level <= 4) {
+        min = 4;
+        max = 5;
+      } else if (level <= 6) {
+        min = 5;
+        max = 7;
+      } else if (level <= 8) {
+        min = 6;
+        max = 8;
+      } else if (level <= 10) {
+        min = 7;
+        max = 10;
+      } else {
+        min = 8;
+        max = 12;
+      }
+      enemiesNumberRange.push(min, max);
+      return enemiesNumberRange;
+    }
+    calculateEnemiesSpeed(level) {
+      const enemiesSpeedRange = [];
+      let min;
+      let max;
+      if (level <= 2) {
+        min = 30;
+        max = 100;
+      } else if (level <= 4) {
+        min = 40;
+        max = 140;
+      } else if (level <= 6) {
+        min = 50;
+        max = 180;
+      } else if (level <= 8) {
+        min = 60;
+        max = 220;
+      } else if (level <= 10) {
+        min = 70;
+        max = 260;
+      } else {
+        min = 80;
+        max = 300;
+      }
+      enemiesSpeedRange.push(min, max);
+      return enemiesSpeedRange;
+    }
   }
-}
+  return Level;
+})();
+
 
 class GamePanel {
   constructor() {
@@ -506,4 +582,5 @@ class GamePanel {
   }
 }
 
-const game = new Game();
+//const game = new Game();
+const game = new arcadeGame();
