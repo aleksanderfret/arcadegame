6709b8771028bc4bc8463056281658767909a19b
Aleksander Fret <alemik@op.pl>
2018-04-29 21:09:39 +0200

    Edit: eslint conf and gitignore file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   "extends": "airbnb-base",
   "rules": {
-    "linebreak-style": ["error", "windows"],
     "object-curly-newline": ["error", {
       "ObjectExpression": {
         "multiline": true,
